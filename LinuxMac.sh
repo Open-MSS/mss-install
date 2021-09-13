a7fb18d5ef1ba41cd133bf78e2500ae5c73fce9e
May <baermay98@gmail.com>
2021-09-13 12:54:42 +0200

    Add memory check, use which conda

--- LinuxMac.sh
+++ LinuxMac.sh
@@ -22,15 +22,22 @@
 #   limitations under the License.
 
 #!/bin/bash -i
+set -euo pipefail
 automatic=$(([ "$1" = "a" ] || [ "$1" = "-a" ]) && echo "Yes" || echo "No")
 echo "Checking Conda installation..."
 condaInstalled="Yes"
-conda || { echo "Downloading miniconda3..." &&
+unameOS=$([ "$(uname -s)" == "Darwin" ] && echo "MacOSX" || echo "Linux")
+dfCommand=$([ $unameOS = "MacOSX" ] && echo "df -g ." || echo "df -BG .")
+completeSize=3
+mssSize=2.7
+
+which conda || { echo "Downloading miniconda3..." &&
    condaInstalled="No" &&
-   unameOS=$([ "$(uname -s)" == "Darwin" ] && echo "MacOSX" || echo "Linux") &&
+   { awk -v n1="$($dfCommand | tail -1 | awk '{print $4+0}')" "BEGIN {exit !(n1 < $completeSize)}" && echo "Aborting. You need at least $completeSize GB of space to install conda and MSS" && exit 0; }
    curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-${unameOS}-x86_64.sh" --output miniconda-installer.sh &&
    echo "Installing miniconda3..." &&
    chmod +x miniconda-installer.sh &&
+
    if [[ $unameOS = "MacOSX" ]]
    then
       if [[ $automatic  = "No" ]]; then script -q output.txt ./miniconda-installer.sh -u; else script -q output.txt ./miniconda-installer.sh -u -b -p ~/miniconda3; fi
@@ -40,10 +47,11 @@ conda || { echo "Downloading miniconda3..." &&
    location=$(cat output.txt | grep "PREFIX=" | tr -d [:cntrl:] | sed -e "s/.*PREFIX=//g") && rm output.txt &&
    if [[ $location != "" ]]; then . $location/etc/profile.d/conda.sh; else . ~/.bashrc; fi && conda init &&
    if [[ $SHELL = *zsh ]]; then conda init zsh; fi && rm miniconda-installer.sh &&
-   conda || { echo "Conda still not found, please restart your console and try again"; exit 1; }
+   which conda || { echo "Conda still not found, please restart your console and try again"; exit 1; }
 }
 
 echo "Conda installed"
+{ awk -v n1="$($dfCommand | tail -1 | awk '{print $4+0}')" "BEGIN {exit !(n1 < $mssSize)}" && echo "Aborting. You need at least $mssSize GB of space to install MSS" && exit 0; } ;
 . $(conda info --base)/etc/profile.d/conda.sh
 conda config --add channels conda-forge
 conda activate mssenv || {
@@ -54,7 +62,7 @@ conda activate mssenv || {
 
 echo "Installing mss..."
 mamba install mss python -y
-mamba list -f mss | grep "conda-forge" || { echo MSS was not successfully installed, aborting; exit 1; }
+mamba list -f mss | grep "conda-forge" || { echo "MSS was not successfully installed, aborting"; exit 1; }
 
 echo "Done!"
 if [[ $condaInstalled = "No" ]]; then echo "Please restart your shell for changes to take effect! 'exec $SHELL'"; fi
