1d2fbc814dadd89cd1ea90f8b1c595ce50117d30
ReimarBauer <rb.proj@gmail.com>
2023-03-19 12:55:34 +0100

    same line as on installation

--- LinuxMac.sh
+++ LinuxMac.sh
@@ -90,8 +90,7 @@ echo "mamba installed"
     if [ $freespace -lt $mssSize ]; then
             echo "Aborting. You need at least $mssSize GB of space to install mamba and MSS" && exit 1;
     fi &&
-    eval "$($location/bin/conda shell.bash hook)" # make sure that the hook is available
-    mamba init
+    if [[ "$location" != "" ]]; then . "$location/etc/profile.d/conda.sh" && . "$location/etc/profile.d/mamba.sh"; else . "$HOME/.bashrc"; fi && mamba init &&
     mamba activate mssenv || {
     echo "mssenv not found, creating..." &&
     mamba create -n mssenv -y &&
