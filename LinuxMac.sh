b8b909aaf69953403df925da9506f3ae28c29ec0
ReimarBauer <rb.proj@gmail.com>
2023-03-21 06:57:38 +0100

    base installation on mambaforge, skip when anaconda/miniconda is found (#9)
    * base installation on mambaforge, skip when anaconda/miniconda is found
    
    * updated workflow
    
    * parameter for shell added
    
    * next trial
    
    * trying su -
    
    * trying -ileo
    
    * trying sudo
    
    * deactivated size check
    
    * more output
    
    * force curl to follow redirection
    
    * check past mamba was installed#
    
    * addresses unbound variable
    
    * call conda.sh before mamba.sh
    
    * change exit code to 0
    
    * trying mamba activate
    
    * path for cmd added
    
    * sudo command
    
    * try mamba init
    
    * path added
    
    * reload bashrc
    
    * use source command
    
    * call bash again
    
    * trying pure activate
    
    * avoid none interactive shell here
    
    * just call msui
    
    * tried same test with macosx
    
    * improved size check
    
    * mac df call fixed
    
    * condition for MacOSC
    
    * old cmd for MacOSX
    
    * option for mac removed
    
    * improved starting message
    
    linted
    
    * sizes adjusted
    
    * sizes adjusted
    
    * RegisterPython enabled
    
    * windows arguments changed
    
    * use option add to path
    
    * status installation
    
    * InstallationType added
    
    * request more info
    
    * trying an older version
    
    * direct call
    
    * another
    
    * test
    
    * revert to the version which runs locally
    
    * another try
    
    * try to install mambaforge from the CI
    
    * changes servercore
    
    * name change
    
    * trying to install from the runner
    
    * next
    
    * ideally we have it already in the path
    
    * no option for now
    
    * in correct dir, lets see
    
    * trying conda activate
    
    * undo change of command
    
    * mamba init added
    
    * next investigation
    
    * back to cmd
    
    * call
    
    * typo
    
    * improved by the ideas from https://github.com/conda-forge/miniforge/issues/421
    
    * Empty-Commit
    
    * typo
    
    * skip cd workspace
    
    cd : Cannot find path 'D:\a\mss-install\mss-install\%GITHUB_WORKSPACE%' because it does not exist.
    
    * L0
    
    calling fish scripts
    
    * -o needed
    
    * additional pwsh commands
    
    * only setlocation
    
    * use cmd.exe
    
    * call mamba info
    
    * try cmd.exe
    
    * trying further cmd.exe calls
    
    * /k option
    
    * remove silent mode
    
    * silent mode seems to suppress the UI start
    
    * back to the start for install-windows
    
    * option D added
    
    * required as last argument
    
    * another windows action
    
    * uppercase env var
    
    * typo
    
    * remove cd
    
    * env in powershell
    
    * powershell env var
    
    * cmd installer added
    
    * typo
    
    * same install command
    
    * fixed exit code
    
    * further test for windows
    
    * adjusted counter
    
    * better names for the actions
    
    * better names
    
    * install-mss-on-existing-linux-mambaforge-installation
    
    * name updated
    
    * added profile.d setup
    
    * source  command
    
    * new bash
    
    * set path
    
    * typo
    
    * sudo
    
    * direct call
    
    * hook added
    
    * without sudo
    
    * sudo before command
    
    * interactive mode removed
    
    * mamba init
    
    * mamba shell.bash hook not yet supported
    
    * mamba init
    
    * verify init
    
    * try conda init
    
    * bash added
    
    * two commands
    
    * reload bashrc
    
    * path added
    
    * lines from linuxscript
    
    * hook added
    
    * comparison improved
    
    * improved
    
    * hook added
    
    * same line as on installation
    
    * another one
    
    * typo
    
    * next
    
    * install-mss-on-existing-macos-mambaforge-installation
    
    * right package
    
    * update
    
    * typo
    
    * update mamba in base
    
    * fixed no parameter
    
    * improved
    
    * if
    
    * scheduled regularly
    
    this lets us find conda/mamba changes
    
    and problems with our released packacke
    
    * improved
    
    * path added
    
    * sudo
    
    * eval
    
    * copy from linux script
    
    * typo

--- LinuxMac.sh
+++ LinuxMac.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 #   mss-install.LinuxMac
 #   ~~~~~~~~~~~~~~~~~~~~~
 #
-#   This script tries to install conda and/or mss on a Linux or MacOS system automatically.
+#   This script tries to install mamba and/or mss on a Linux or MacOS system automatically.
 #
 #   This file is part of MSS.
 #
 #   :copyright: Copyright 2021 May Baer
-#   :copyright: Copyright 2021-2022 by the MSS team, see AUTHORS.
+#   :copyright: Copyright 2021-2023 by the MSS team, see AUTHORS.
 #   :license: APACHE-2.0, see LICENSE for details.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,53 +21,93 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
-#!/bin/bash -i
 set -euo pipefail
-automatic=$(([ "$1" = "a" ] || [ "$1" = "-a" ]) && echo "Yes" || echo "No")
-echo "Checking Conda installation..."
-condaInstalled="Yes"
+automatic="No"
+if [[ "$#" == 1 ]]; then
+    automatic=$({ [ "$1" = "a" ] || [ "$1" = "-a" ]; } && echo "Yes" || echo "No")
+fi
+echo "Checking Mamba installation..."
 unameOS=$([ "$(uname -s)" == "Darwin" ] && echo "MacOSX" || echo "Linux")
-if [ $(uname -m) == "arm64" ] && [ $unameOS == "MacOSX" ]; then echo "Aborting. Mac ARM M1 currently not supported." && exit 1; fi
-dfCommand=$([ $unameOS = "MacOSX" ] && echo "df -g ." || echo "df -BG .")
-completeSize=3
-mssSize=2.7
+architectureOS="$(uname -m)"
+if [ "$(uname -m)" == "arm64" ] && [ "$unameOS" == "MacOSX" ]; then echo "Aborting. Mac ARM M1 currently not supported." && exit 1; fi
+completeSize=4 # integer
+mssSize=2 # integer
 
-which conda || { echo "Downloading miniconda3..." &&
-   condaInstalled="No" &&
-   { awk -v n1="$($dfCommand | tail -1 | awk '{print $4+0}')" "BEGIN {exit !(n1 < $completeSize)}" && echo "Aborting. You need at least $completeSize GB of space to install conda and MSS" && exit 0; }
-   curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-${unameOS}-x86_64.sh" --output miniconda-installer.sh &&
-   echo "Installing miniconda3..." &&
-   chmod +x miniconda-installer.sh &&
+echo "
+We recommend to start from Mambaforge for the MSS installation.
+
+Mambaforge comes with the popular conda-forge channel preconfigured, but you can modify the configuration to use any channel you like.
+
+The next steps are to check for an existing Mambaforge Installation.
+
+If possible we try to:
+
+install MambaForge including mamba then Create a mssenv then Install MSS.
+"
+
+CHECKCONDA=$(which conda)
+if [[ $CHECKCONDA == *"miniconda"* ]] || [[ $CHECKCONDA == *"anaconda"* ]]; then
+ echo "Found a anaconda/miniconda installation see documentation for a manual installation";
+ exit 1;
+fi
+
+
+sleep 2
+
+
+# neither conda nor mamba -> mambaforge
+which mamba || { echo "Downloading mambaforge..." &&
+    if [[ $unameOS = "MacOSX" ]]
+       then
+          freespace=$(df -g  . |  tail -1 | awk '{print $4+0}')
+       else
+          freespace=$(df -BG --output='avail' . | tail -1 | awk '{print $1+0}') 
+    fi &&
+    if [[ $freespace -lt $completeSize ]]; then
+	    echo "Aborting. You need at least $completeSize GB of space to install mamba and MSS" && exit 1;
+    fi &&
+   curl -L0 "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-${unameOS}-${architectureOS}.sh" --output mambaforge-installer.sh &&
+   ls -l mambaforge-installer.sh &&
+   echo "Installing mambaforge..." &&
+   chmod +x mambaforge-installer.sh &&
 
    if [[ $unameOS = "MacOSX" ]]
    then
-      if [[ $automatic  = "No" ]]; then script -q output.txt ./miniconda-installer.sh -u; else script -q output.txt ./miniconda-installer.sh -u -b -p ~/miniconda3; fi
+      if [[ $automatic  = "No" ]]; then script -q output.txt ./mambaforge-installer.sh -u; else script -q output.txt ./mambaforge-installer.sh -u -b -p ~/mambaforge; fi
    else
-      if [[ $automatic  = "No" ]]; then script -q -c "./miniconda-installer.sh -u" output.txt; else script -q -c "./miniconda-installer.sh -u -b -p ~/miniconda3" output.txt; fi
+      if [[ $automatic  = "No" ]]; then script -q -c "./mambaforge-installer.sh -u" output.txt; else script -q -c "./mambaforge-installer.sh -u -b -p ~/mambaforge" output.txt; fi
    fi && cat output.txt &&
-   location=$(cat output.txt | grep "PREFIX=" | tr -d [:cntrl:] | sed -e "s/.*PREFIX=//g") && rm output.txt &&
-   if [[ $location != "" ]]; then . $location/etc/profile.d/conda.sh; else . ~/.bashrc; fi && conda init &&
-   if [[ $SHELL = *zsh ]]; then conda init zsh; fi && rm miniconda-installer.sh &&
-   which conda || { echo "Conda still not found, please restart your console and try again"; exit 1; }
+   location=$(< output.txt grep "PREFIX=" | tr -d "[:cntrl:]" | sed -e "s/.*PREFIX=//g") && rm output.txt &&
+   if [[ "$location" != "" ]]; then . "$location/etc/profile.d/conda.sh" && . "$location/etc/profile.d/mamba.sh"; else . "$HOME/.bashrc"; fi && mamba init &&
+   if [[ "$SHELL" = *zsh ]]; then conda init zsh; fi && rm mambaforge-installer.sh &&
+   which mamba || { echo "mamba still not found, please restart your console and try again"; exit 1; }
 }
 
-echo "Conda installed"
-{ awk -v n1="$($dfCommand | tail -1 | awk '{print $4+0}')" "BEGIN {exit !(n1 < $mssSize)}" && echo "Aborting. You need at least $mssSize GB of space to install MSS" && exit 0; } ;
-. $(conda info --base)/etc/profile.d/conda.sh
-conda config --add channels conda-forge
-conda activate mssenv || {
+echo "mamba installed"
+    if [[ $unameOS = "MacOSX" ]]
+       then
+          freespace=$(df -g  . | tail -1 | awk '{print $4+0}') 
+       else
+          freespace=$(df -BG --output='avail' . | tail -1 | awk '{print $1+0}') 
+    fi &&
+    if [[ $freespace -lt $mssSize ]]; then
+            echo "Aborting. You need at least $mssSize GB of space to install mamba and MSS" && exit 1;
+    fi &&
+    . "$HOME/mambaforge/etc/profile.d/conda.sh" &&
+    . "$HOME/mambaforge/etc/profile.d/mamba.sh" &&
+    mamba init &&
+    mamba update -n base mamba && # update mamba to the newest version
+    mamba activate mssenv || {
     echo "mssenv not found, creating..." &&
-    conda create -n mssenv mamba -y &&
-    conda activate mssenv || { echo "Environment not found, aborting"; exit 1; }
+    mamba create -n mssenv -y &&
+    mamba activate mssenv || { echo "'mssenv' environment could not be created, aborting"; exit 1; }
 }
 
-echo "Installing MSS..."
-mamba install mss python -y
-mamba list -f mss | grep "conda-forge" || { echo "MSS was not successfully installed, aborting"; exit 1; }
+ echo "Installing MSS..."
+ mamba install mss python -y
+ mamba list -f mss | grep "conda-forge" || { echo "MSS was not successfully installed, aborting"; exit 1; }
+ echo "To start msui from the MSS Software,"
+ echo "1. Activate your mamba environment with this command: 'mamba activate mssenv'"
+ echo "2. Start msui with this command: 'msui'"
+ exit 0;
 
-echo "Done!"
-if [[ $condaInstalled = "No" ]]; then echo "Please restart your shell for changes to take effect! 'exec $SHELL'"; fi
-echo "To start msui from the MSS Software,"
-echo "1. Activate your conda environment with this command: 'conda activate mssenv'"
-echo "2. Start msui with this command: 'msui'"
--- Windows.bat
+++ Windows.bat
@@ -1,12 +1,12 @@
 ::    mss-install.Windows
-::    ~~~~~~~~~~~~~~~~~~~~~
+::    ~~~~~~~~~~~~~~~~~~
 ::
-::    This script tries to install conda and/or mss on a Windows system automatically.
+::    This script tries to install mamba and/or mss on a Windows system automatically.
 ::
 ::    This file is part of MSS.
 ::
 ::    :copyright: Copyright 2021 May Baer
-::    :copyright: Copyright 2021-2022 by the MSS team, see AUTHORS.
+::    :copyright: Copyright 2021-2023 by the MSS team, see AUTHORS.
 ::    :license: APACHE-2.0, see LICENSE for details.
 ::
 ::    Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,60 +25,92 @@
 if "%1"=="a" (set automatic=a) else (if "%1"=="-a" (set automatic=a) else (set automatic=No))
 if "%2"=="retry" (set retry=retry) else (set retry=No)
 set script=%~f0
-echo Checking Conda installation...
-where conda
-if %errorlevel% == 0 (goto condainstalled)
 
-:installconda
-echo Checking existing conda installs not in path...
+echo We recommend to start from Mambaforge for the MSS installation.
+echo Mambaforge comes with the popular conda-forge channel preconfigured,
+echo but you can modify the configuration to use any channel you like.
+echo The next steps are to check for an existing Installation.
+echo If possible we try to:
+echo install MambaForge including mamba then Create a mssenv then Install MSS.
+
+
+echo Checking existing Anaconda/Miniconda installs not in path...
+echo ============================================================
 set a="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Anaconda Prompt (anaconda3).lnk"
 set b="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Anaconda Prompt (miniconda3).lnk"
 set c="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Anaconda3 (32-bit)\Anaconda Prompt (anaconda3).lnk"
 set d="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Anaconda3 (32-bit)\Anaconda Prompt (miniconda3).lnk"
-if exist %a% if "%3" neq "a" if "%3" neq "b" if "%3" neq "c" if "%3" neq "d" call "cmd /c" %a% "& %script:"=% %automatic% %retry% a" & exit 0
-if exist %b% if "%3" neq "b" if "%3" neq "c" if "%3" neq "d" call "cmd /c" %b% "& %script:"=% %automatic% %retry% b" & exit 0
-if exist %c% if "%3" neq "c" if "%3" neq "d" call "cmd /c" %c% "& %script:"=% %automatic% %retry% c" & exit 0
-if exist %d% if "%3" neq "d" call "cmd /c" %d% "& %script:"=% %automatic% %retry% d" & exit 0
 
+if exist %a% if "%3" neq "a" if "%3" neq "b" if "%3" neq "c" if "%3" neq "d" (goto showmanualhint)
+if exist %b% if "%3" neq "b" if "%3" neq "c" if "%3" neq "d" (goto showmanualhint)
+if exist %c% if "%3" neq "c" if "%3" neq "d" (goto showmanualhint)
+if exist %d% if "%3" neq "d" (goto showmanualhint)
+
+echo Checking Mamba installation...
+echo ==============================
+where mamba
+if %errorlevel% == 0 (goto mambainstalled)
+
+
+:installmamba
 wmic logicaldisk where DeviceID='%USERPROFILE:~0,2%' get FreeSpace > space.txt
 for /f "delims= " %%i in ('type space.txt') do set space=%%i
 del space.txt
 set "spaceMB=%space:~,-6%"
-if %spaceMB% LSS 3221 (echo You need at least 3GB of space to install conda and MSS, aborting. & pause & exit /B 1)
-echo Downloading miniconda3...
-if "%retry%"=="retry" (echo Conda still not found after installation, aborting & pause & exit /B 1)
+if %spaceMB% LSS 3221 (echo You need at least 3GB of space to install mamba and MSS, aborting. & pause & exit /B 1)
+echo Downloading mambaforge...
+echo =========================
+
+if "%retry%"=="retry" (echo Mamba still not found after installation, aborting & pause & exit /B 1)
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSBIT=32BIT || set OSBIT=64BIT
-if %OSBIT%==32BIT curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86.exe --output miniconda-installer.exe
-if %OSBIT%==64BIT curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe --output miniconda-installer.exe
-echo Installing miniconda3 (just use the default settings if you are unsure)...
-if "%automatic%"=="a" (start /wait miniconda-installer.exe /S /D=%USERPROFILE%\miniconda3) else (start /wait miniconda-installer.exe)
-del "miniconda-installer.exe"
+if %OSBIT%==64BIT curl -L0  https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Windows-x86_64.exe --output mambaforge.exe
+echo Installing mambaforge (Enable: Register Mambaforge as my default Python and add it to the path
+echo ==============================================================================================
+
+if "%automatic%"=="a" (start /wait "" mambaforge.exe /InstallationType=JustMe /RegisterPython=1 /AddToPath=1 /S /D=%USERPROFILE%\mambaforge) else (start /wait "" mambaforge-installer.exe)
+
+del "mambaforge.exe"
+
 start /i /b %script% %automatic% retry & exit 0
 
-:condainstalled
-echo Conda installed
+
+:mambainstalled
+echo Mamba installed
+echo ===============
 wmic logicaldisk where DeviceID='%USERPROFILE:~0,2%' get FreeSpace > space.txt
 for /f "delims= " %%i in ('type space.txt') do set space=%%i
 del space.txt
 set "spaceMB=%space:~,-6%"
 if %spaceMB% LSS 2899 (echo You need at least 2.7GB of space to install MSS, aborting. & pause & exit /B 1)
-call conda.bat config --add channels conda-forge
-call conda.bat activate mssenv
+call mamba.bat update -n base mamba && :: update mamba to the newest version
+call mamba.bat activate mssenv
 if not %errorlevel% == 0 (
     echo mssenv not found, creating...
-    call conda.bat create -n mssenv mamba -y
-    call conda.bat activate mssenv
+    call mamba.bat create -n mssenv -y
+    call mamba.bat activate mssenv
     if errorlevel 1 (echo Environment not found, aborting & pause & exit /B 1)
 )
 
 :envexists
 echo Installing MSS...
+echo =================
 call mamba install mss python -y
 call mamba list -f mss| findstr /i /e "conda-forge"
 if not %errorlevel% == 0 (echo MSS was not successfully installed, aborting & pause & exit /B 1)
 
 echo Done! To start msui from the MSS Software: Press the Windows button, type in "msui" and press enter.
 echo Alternatively:
-echo 1. Activate your conda environment with this command: "conda activate mssenv"
+echo 1. Activate your mamba environment with this command: "mamba activate mssenv"
 echo 2. Start msui with this command: "msui"
+
 pause
+
+exit /B 0
+
+:showmanualhint
+echo Found a anaconda/miniconda installation see documentation for a manual installation
+echo ===================================================================================
+
+pause
+
+exit /B 1
